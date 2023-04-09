5af4b1b3497edf8497c9fe1922078643ab198025
Emirhan Topal <emirhantopal03@hotmail.com>
2023-04-09 22:18:04 +0300

    fix bugs

--- U85-Game/Assets/Scripts/PlatformController.cs
+++ U85-Game/Assets/Scripts/PlatformController.cs
@@ -11,27 +11,11 @@ using Utils.RefValue;
 
 public class PlatformController : MonoBehaviour
 {
-
-    
-    public GameObject pivot;
+    [SerializeField] private Transform pivot;
 
     [SerializeField] private bool _Isleft;
 
-    private bool _canRotate = true;
-
-    private float currentRotation;
-    private float deltaRotationLeft;
-    private float deltaRotationRight;
-
-    private void Awake()
-    {
-        currentRotation = pivot.transform.localRotation.eulerAngles.z;
-        deltaRotationLeft = Mathf.DeltaAngle(currentRotation, 30f);
-        deltaRotationRight = Mathf.DeltaAngle(currentRotation, -30f);
-    }
-
     
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         col.TryGetComponent(out BallEmpty _Ball);
@@ -39,45 +23,13 @@ public class PlatformController : MonoBehaviour
         {
             if (_Isleft)
             {
-                const float maxRotation = 30f;
-                const float minRotation = -60f;
-
-                currentRotation = pivot.transform.localRotation.eulerAngles.z;
-                // float deltaRotation = Mathf.DeltaAngle(currentRotation, maxRotation);
-
-                if (deltaRotationLeft > -30f)
-                {
-                    float rotationAmount = Mathf.Min(-deltaRotationLeft, 10f);
-                    float newRotation = Mathf.Clamp(currentRotation + 10f, minRotation, maxRotation);
-                    pivot.transform.rotation = Quaternion.Euler(0f, 0f, newRotation);
-                }
-
-                Debug.Log("ss");
+                if(pivot.rotation.eulerAngles.z < 10 || pivot.rotation.eulerAngles.z > 345)
+                    pivot.Rotate(0,0,5);
             }
             else
             {
-                const float maxRotation = 0f;
-                const float minRotation = -30f;
-
-                currentRotation = pivot.transform.localRotation.eulerAngles.z;
-                // float deltaRotation = Mathf.DeltaAngle(currentRotation, minRotation);
-
-                if (deltaRotationRight < 30f)
-                {
-                    float rotationAmount = Mathf.Min(deltaRotationRight, -10f);
-                    // float rotationAmount = Mathf.Min(-deltaRotation, 10f);
-                    float newRotation = Mathf.Clamp(currentRotation - 10f, minRotation, maxRotation);
-                    
-                    
-                    // if (newRotation <= minRotation) // MinRotation'a ulaşıldığında dönüş yönünü tersine çevir
-                    // {
-                    //     var f = 10f;
-                    //     f *= -1f;
-                    // }
-                    pivot.transform.rotation = Quaternion.Euler(0f, 0f, newRotation);
-                }
-
-                Debug.Log("aa");
+                if(pivot.rotation.eulerAngles.z < 15 || pivot.rotation.eulerAngles.z > 350)
+                    pivot.Rotate(0,0,-5);
             }
 
             LeanPool.Despawn(_Ball.gameObject);
